52c36ecc03fb63f1430b656094b05e10fe182233
vsuzdaltsev <suzdaltsev_v@mail.ru>
2020-12-23 23:16:02 +0300

    lambda: upload_to_s3_bucket

--- upload_to_s3_bucket/handler.py
+++ upload_to_s3_bucket/handler.py
@@ -3,12 +3,16 @@ import uuid
 import boto3
 
 
+EXPIRATION = 3600
+
+
 def upload_to_s3_bucket(event, _context):
     client = boto3.client('s3')
     upload_key = uuid.uuid4().hex
     bucket = event.get('bucket_name')
     presigned_url = client.generate_presigned_url(
         ClientMethod='put_object',
+        ExpiresIn=EXPIRATION,
         Params={
             'Bucket': bucket,
             'Key': upload_key
