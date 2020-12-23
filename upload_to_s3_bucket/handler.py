2650465f4cf0d540d7473dcd9ea997ff9f5effda
vsuzdaltsev <suzdaltsev_v@mail.ru>
2020-12-23 23:02:09 +0300

    lambda: upload to s3 bucket

--- upload_to_s3_bucket/handler.py
+++ upload_to_s3_bucket/handler.py
@@ -1,15 +1,18 @@
-import json
-from logging import Logger
+import uuid
 
 import boto3
 
 
 def upload_to_s3_bucket(event, _context):
+    client = boto3.client('s3')
+    upload_key = uuid.uuid4().hex
+    bucket = event.get('bucket_name')
+    presigned_url = client.generate_presigned_url(
+        ClientMethod='put_object',
+        Params={
+            'Bucket': bucket,
+            'Key': upload_key
+        }
+    )
 
-
-    return {
-        "status_code": 200,
-        "input": event,
-        "uploaded": True,
-        "error": 'error'
-    }
+    return {"upload_url": presigned_url}
