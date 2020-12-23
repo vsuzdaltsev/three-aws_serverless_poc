1b277d1517bf2b803d4e6d86abea59ce910b1a05
vsuzdaltsev <suzdaltsev_v@mail.ru>
2020-12-23 20:38:15 +0300

    cleanup

--- create_s3_bucket/handler.py
+++ create_s3_bucket/handler.py
@@ -5,27 +5,27 @@ import boto3
 
 
 def create_s3_bucket(event, _context):
-    region = event['region'] if event['region'] and event['region'] != 'us-east-1' else None
-    bucket_name = event['bucket_name']
-
     def log(severity, message):
         Logger(name='__name__').__getattribute__(severity)(message)
 
-    def create():
+    def create(region, bucket_name):
         try:
             if region is None:
-                s3_client = boto3.client('s3')
-                s3_client.create_bucket(Bucket=bucket_name)
+                client = boto3.client('s3')
+                client.create_bucket(Bucket=bucket_name)
             else:
-                s3_client = boto3.client('s3', region_name=region)
+                client = boto3.client('s3', region_name=region)
                 location = {'LocationConstraint': region}
-                s3_client.create_bucket(Bucket=bucket_name, CreateBucketConfiguration=location)
+                client.create_bucket(Bucket=bucket_name, CreateBucketConfiguration=location)
         except BaseException as err:
             log("error", f">> Failed while creating s3 bucket {bucket_name}. Error: {err}")
             return False, str(err)
         return True, None
 
-    created, error = create()
+    where = event['region'] if event['region'] and event['region'] != 'us-east-1' else None
+    bucket = event['bucket_name']
+
+    created, error = create(region=where, bucket_name=bucket)
 
     return {
         "status_code": 200,
