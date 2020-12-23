2ef5a042049c14c50d15deffc77b1c34ef2c96dd
vsuzdaltsev <suzdaltsev_v@mail.ru>
2020-12-23 22:52:33 +0300

    lambda: fx create_s3_bucket

--- create_s3_bucket/handler.py
+++ create_s3_bucket/handler.py
@@ -9,19 +9,21 @@ def create_s3_bucket(event, _context):
 
     def create(region, bucket_name):
         try:
-            if region is None:
+            if region in [None, 'us-east-1']:
                 client = boto3.client('s3')
                 client.create_bucket(Bucket=bucket_name)
             else:
                 client = boto3.client('s3', region_name=region)
                 location = {'LocationConstraint': region}
                 client.create_bucket(Bucket=bucket_name, CreateBucketConfiguration=location)
+
+            client.get_waiter('bucket_exists')
         except BaseException as err:
             log("error", f">> Failed while creating s3 bucket {bucket_name}. Error: {err}")
             return False, str(err)
         return True, None
 
-    where = event['region'] if event['region'] and event['region'] != 'us-east-1' else None
+    where = event['region']
     bucket = event['bucket_name']
 
     created, error = create(region=where, bucket_name=bucket)
