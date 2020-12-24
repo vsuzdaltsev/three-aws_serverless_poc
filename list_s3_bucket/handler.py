8c6bbf56888ea47b9812a8edc8d775ed8beba8d4
Raman Kisliak <rkisliak@exadel.com>
2020-12-24 16:39:55 +0300

    fixes

--- list_s3_bucket/handler.py
+++ list_s3_bucket/handler.py
@@ -6,21 +6,28 @@ import boto3
 def list_s3_bucket(event, _context):
     """
     Payload example:
-        https://uri/{bucket_name}
+        curl -X GET http://{endpoint_uri}/prod/poc/list_s3_bucket/{bucket_name} -H "x-api-key: {api_access_key}"
     """
     def log(severity, message):
         Logger(name='__name__').__getattribute__(severity)(message)
 
     def list_files(bucket_name):
-      s3 = boto3.resource('s3')
-      bucket = s3.Bucket(bucket_name)
+        try:
+            if lambda_input:
+                log("info", f">> Processing listing files for s3 bucket {bucket_name}")
+                s3 = boto3.resource('s3')
+                bucket = s3.Bucket(bucket_name)
+                aggr = []
+                for file in bucket.objects.all():
+                    aggr.append(file.key)
+                return aggr, None
+            else:
+                log("warning", f">> No bucket name provided")
+        except BaseException as err:
+            log("error", f">> Failed while listing s3 bucket {bucket_name}. Error: {err}")
+            return False, str(err)
+        return True, None
 
-      aggr = []
-
-      for file in bucket.objects.all():
-          aggr.append(file.key)
-      return aggr, None
-    
     lambda_input = event.get('path').split('/')[-1]
     output, error = list_files(bucket_name=lambda_input)
 
