800d8b72822f47f80fea4fe7d4d4d5a6a98812f2
vsuzdaltsev <suzdaltsev_v@mail.ru>
2020-12-25 06:42:28 +0300

    disclaimer

--- demo.py
+++ demo.py
@@ -21,6 +21,8 @@ if __name__ == "__main__":
     REGION = 'us-east-1'
     TEST_BUCKET = 'uniqnamedbucket5'
     API_KEY = os.getenv('TEST_POC_API_KEY')
+    assert isinstance(API_KEY, dict), ">> Please export appropriate TEST_POC_API_KEY"
+
     BASE_URI = 'http://localhost:3000/prod/poc'
     HEADERS = {'x-api-key': API_KEY}
 
