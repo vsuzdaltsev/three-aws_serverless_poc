9d5b4e0e09a728d3986d441377be92319c432624
vsuzdaltsev <suzdaltsev_v@mail.ru>
2020-12-26 14:39:31 +0300

    dockerize sls operations

--- tasks.py
+++ tasks.py
@@ -15,40 +15,22 @@ def log(severity, message):
     Logger(name='__name__').__getattribute__(severity)(message)
 
 
-def sls_config(key):
-    with open('serverless.yml', 'r') as config:
-        file = config.read()
-        return yaml.safe_load(file).get(key)
-
-
 @task
-def install_dependencies(c):
-    """Install necessary serverless plugins."""
-    plugins = sls_config('plugins')
-
-    def install_sls_plugin(plugin):
-        log("warning", f">> Install dependencies: {plugin}")
-        c.run(f"sls plugin install --name {plugin}")
-
-    list(map(lambda p: install_sls_plugin(p), plugins))
-
-
-@task(pre=[install_dependencies])
 def deploy(c):
     """>> Deploy serverless application."""
-    c.run('sls deploy')
+    c.run(f"{IN_DOCKER} sls deploy")
 
 
 @task
 def remove(c):
     """>> Remove serverless application."""
-    c.run('sls remove')
+    c.run(f"{IN_DOCKER} sls remove")
 
 
 @task
 def info(c):
     """>> Serverless application status info. Including endpoints and api token."""
-    c.run(f"sls info")
+    c.run(f"{IN_DOCKER} sls info")
 
 
 @task
